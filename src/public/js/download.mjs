35871434b02bdbc9350f51aa930a2ec8f6f7c37f
FabioDevCode <0677384849@orange.fr>
2024-10-29 00:04:03 +0100

    Add - Drag & Drop Zone to Update list

--- src/public/js/download.mjs
+++ src/public/js/download.mjs
@@ -6,7 +6,7 @@ const showList = () => {
     let content = '';
 
     for (const link of arrayList) {
-        content += `<li><div><h3>${link.title}</h3><p>${link.url}</p></div><div class="trash-list"><button data-trash="" data-trash-url="${link.url}"><i class="fas fa-times"></i></button></div></li>`;
+        content += `<li><div><h3>${link.title}</h3><p>${link.url}</p></div><div class="trash-list"><button data-trash="" data-trash-title="${link.title}"><i class="fas fa-times"></i></button></div></li>`;
     }
     ulAllList.innerHTML = content;
     content = '';
@@ -14,17 +14,10 @@ const showList = () => {
 
     for (const trash_btn of allTrashBtn) {
         trash_btn.addEventListener('click', function() {
-            const urlToRemove = this.getAttribute('data-trash-url');
-
-            console.log(urlToRemove);
-
+            const titleToRemove = this.getAttribute('data-trash-title');
             const arrayList = JSON.parse(localStorage.getItem("slyhear-list"));
-            const updatedArrayList = arrayList.filter(item => item.url !== urlToRemove);
-
-            console.log(updatedArrayList);
-
+            const updatedArrayList = arrayList.filter(item => item.title !== titleToRemove);
             localStorage.setItem("slyhear-list", JSON.stringify(updatedArrayList));
-
             showList();
         })
     };
@@ -32,7 +25,7 @@ const showList = () => {
 
 showList();
 
-document.querySelector('[data-btn-add-list]').addEventListener('click', () => {
+document.querySelector('[btn-add-list]').addEventListener('click', () => {
     const url = document.querySelector('[name="url"]');
     const title = document.querySelector('[name="title"]');
 
@@ -40,22 +33,39 @@ document.querySelector('[data-btn-add-list]').addEventListener('click', () => {
         Toastify({
             text: "Veuillez remplir une URL et un TITRE",
             className: "error",
-            duration: 2500,
+            duration: 3000,
             newWindow: true,
             close: false,
-            gravity: "bottom", // `top` or `bottom`
-            position: "left", // `left`, `center` or `right`
-            stopOnFocus: true, // Prevents dismissing of toast on hover
-            onClick: () => {} // Callback after click
+            gravity: "bottom",
+            position: "left",
+            stopOnFocus: true,
+            onClick: () => {}
         }).showToast();
         return
     }
 
     const arrayList = JSON.parse(localStorage.getItem("slyhear-list"));
 
+    for (const musicObj of arrayList) {
+        if(url.value === musicObj.url) {
+            Toastify({
+                text: "Cette URL est déjà présente dans la liste. Veuillez changer l'URL avant de Valider",
+                className: "warning",
+                duration: 3000,
+                newWindow: true,
+                close: false,
+                gravity: "bottom",
+                position: "left",
+                stopOnFocus: true,
+                onClick: () => {}
+            }).showToast();
+            return
+        }
+    };
+
     arrayList.push({
-        url: url.value,
-        title: title.value
+        url: url.value.trim(),
+        title: title.value.trim()
     });
 
     arrayList.sort((a, b) => {
@@ -69,3 +79,132 @@ document.querySelector('[data-btn-add-list]').addEventListener('click', () => {
 
     showList();
 });
+
+document.querySelector('[btn-clear-list]').addEventListener('click', () => {
+    if(window.confirm('Êtes-vous sûre de vouloir vider la liste ?')) {
+        localStorage.setItem("slyhear-list", JSON.stringify([]));
+        Toastify({
+            text: "Liste vider avec succès !",
+            className: "success",
+            duration: 3000,
+            newWindow: true,
+            close: false,
+            gravity: "bottom",
+            position: "left",
+            stopOnFocus: true,
+            onClick: () => {}
+        }).showToast();
+
+        showList();
+    }
+});
+
+// DRAG AND DROP FILE =============================================== //
+const inputFile = document.querySelector('[input-dropzone]');
+const fileContainer = document.querySelector('[drag-and-drop-zone]');
+
+fileContainer.addEventListener('dragover', (e) => {
+    e.preventDefault();
+});
+
+fileContainer.addEventListener('drop', (e) => {
+    e.preventDefault();
+    if(e.dataTransfer.items.length > 1) {
+        Toastify({
+            text: "Veuillez importer un seul fichier.",
+            className: "error",
+            duration: 5000,
+            newWindow: true,
+            close: true,
+            gravity: "bottom",
+            position: "left",
+            stopOnFocus: true,
+            onClick: () => {}
+        }).showToast();
+    } else if (e.dataTransfer.items.length === 1) {
+        [...e.dataTransfer.items].forEach((item, i) => {
+            if (item.kind === "file") {
+                const file = item.getAsFile();
+                const reader = new FileReader();
+                reader.readAsText(file);
+                reader.onload = () => {
+                    const arrayList = JSON.parse(localStorage.getItem("slyhear-list"));
+                    const newArray = [
+                        ...arrayList,
+                        ...JSON.parse(reader.result)
+                    ];
+                    localStorage.setItem("slyhear-list", JSON.stringify(newArray));
+                    showList();
+                };
+                reader.onerror = () => {
+                    console.error(reader.error);
+                    Toastify({
+                        text: "Une erreur s'est produite, veuillez réessayer plutard.",
+                        className: "error",
+                        duration: 5000,
+                        newWindow: true,
+                        close: true,
+                        gravity: "bottom",
+                        position: "left",
+                        stopOnFocus: true,
+                        onClick: () => {}
+                    }).showToast();
+                }
+            }
+        })
+    }
+});
+
+inputFile.addEventListener('change', (e) => {
+    const file = e.target.files[0] || e.dataTransfer.files[0];
+    const reader = new FileReader();
+    reader.readAsText(file);
+    reader.onload = () => {
+        const arrayList = JSON.parse(localStorage.getItem("slyhear-list"));
+        const newArray = [
+            ...arrayList,
+            ...JSON.parse(reader.result)
+        ];
+        localStorage.setItem("slyhear-list", JSON.stringify(newArray));
+        showList();
+    };
+    reader.onerror = () => {
+        console.error(reader.error);
+        Toastify({
+            text: "Une erreur s'est produite, veuillez réessayer plutard.",
+            className: "error",
+            duration: 5000,
+            newWindow: true,
+            close: true,
+            gravity: "bottom",
+            position: "left",
+            stopOnFocus: true,
+            onClick: () => {}
+        }).showToast();
+    };
+});
+
+document.querySelector('[btn-dl-exemple]').addEventListener('click', () => {
+    const exemple = [
+        {
+            url: "https://www.youtube.com/watch?v=V62xgYWKnJQ",
+            title: "Carpoolboys & Jean Juan - Applause"
+        },
+        {
+            url: "https://www.youtube.com/watch?v=SL_nGCX4SSs",
+            title: "Daft Punk - Television Rules The Nation / Crescendolls (Remake)"
+        }
+    ];
+
+    const dataStr = `data:application/json;charset=utf-8,${encodeURIComponent(JSON.stringify(exemple))}`;
+    const downloadLink = document.querySelector('[link-dl-exemple]');
+    downloadLink.setAttribute('href', dataStr);
+    downloadLink.setAttribute('download', 'Slyhear.json');
+    downloadLink.click();
+});
+
+
+
+
+
+// Faire une fonction de controle du format du JSON.
