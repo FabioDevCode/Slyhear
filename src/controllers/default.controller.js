3e18b5b8b9fdffa6c6711e5bff37c11f3b0cb4a6
FabioDevCode <0677384849@orange.fr>
2024-11-01 19:11:41 +0100

    WIP - library show all sounds

--- src/controllers/default.controller.js
+++ src/controllers/default.controller.js
@@ -1,3 +1,6 @@
+import { preparedTracksToShow } from "../helpers/tracks.helpers.js";
+import models from "../models/index.js";
+
 export const index = (req, res) => {
 	res.redirect("/home");
 };
@@ -6,13 +9,25 @@ export const home = (req, res) => {
 	res.render("index");
 };
 
-export const library = (req, res) => {
-	res.render("library");
+export const library = async (req, res) => {
+	try {
+		const allTracks = await models.Tracks.findAll({
+			attributes: ["id", "title", "imagePath"],
+			raw: true,
+		});
+
+		const tracks = await preparedTracksToShow(allTracks);
+
+		res.render("library", { tracks });
+	} catch (err) {
+		console.error(err);
+		res.redirect("/home");
+	}
 };
 
 export const upload = (req, res) => {
 	res.render("upload");
-}
+};
 
 export const download = (req, res) => {
 	res.render("download");
---
+++ src/helpers/tracks.helpers.js
@@ -0,0 +1,44 @@
+import fs from "node:fs";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const imagesDir = path.join(__dirname, "..", "upload", "images");
+// const soundsDir = path.join(__dirname, "..", "upload", "sounds");
+
+async function getImageBuffer(imagePath) {
+	try {
+		const imageBuffer = await fs.promises.readFile(imagePath);
+		return imageBuffer;
+	} catch (err) {
+		return null;
+	}
+}
+
+export const preparedTracksToShow = async (arrayTracks) => {
+	try {
+		const tracksWithImages = await Promise.all(
+			arrayTracks.map(async (track) => {
+				let imageBuffer = null;
+
+				if (track.imagePath) {
+					const imagePath = path.join(imagesDir, track.imagePath);
+					imageBuffer = await getImageBuffer(imagePath);
+				}
+
+				return {
+					id: track.id,
+					title: track.title,
+					image: imageBuffer ? imageBuffer.toString("base64") : null,
+				};
+			}),
+		);
+
+		return tracksWithImages;
+	} catch (err) {
+		console.error(err);
+		return err;
+	}
+};
