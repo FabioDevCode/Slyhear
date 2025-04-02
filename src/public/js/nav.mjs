13dfc7d68f412c8e5b308ad905da8c15ff817755
FabioDevCode <0677384849@orange.fr>
2025-04-02 20:30:44 +0200

    Update style

--- src/public/js/nav.mjs
+++ src/public/js/nav.mjs
@@ -6,12 +6,14 @@ export const activeMenuBtnAndBreadcrumb = () => {
 		download: "Télécharger",
 	};
 
-	const nav_btns = document.querySelectorAll('[data-nav="nav-btn"]');
-	for (const btn of nav_btns) {
-		btn.classList.remove("active");
-	}
+	if(window.location.pathname !== "/") {
+		const nav_btns = document?.querySelectorAll('[data-nav="nav-btn"]');
+		for (const btn of nav_btns) {
+			btn.classList.remove("active");
+		}
 
-	const firtPath = window.location.pathname.split("/")[1];
-	document.querySelector(`#${firtPath}`).classList.add("active");
-	// document.getElementById("breadcrumb").innerHTML = breadcrumb_map[firtPath];
+		const firtPath = window.location.pathname.split("/")[1];
+		document?.querySelector(`#${firtPath}`).classList.add("active");
+		// document.getElementById("breadcrumb").innerHTML = breadcrumb_map[firtPath];
+	}
 };
