d2c7f69bcc57672d0855d11318da2d81a676281c
FabioDevCode <0677384849@orange.fr>
2025-05-27 22:02:55 +0200

    Add isConnected middleware

--- src/routes/action.routes.js
+++ src/routes/action.routes.js
@@ -3,16 +3,15 @@ const router = express.Router();
 import * as actl from "../controllers/action.controller.js";
 
 import { loginControl } from "../validators/user.validators.js";
+import { isConnected } from "../middlewares/cookie.middlewares.js";
 
-router.get("/download", actl.goDownload);
-router.get("/delete/:id", actl.deleteSound);
-router.get("/stream/:code", actl.setStream);
+router.get("/download", isConnected, actl.goDownload);
+router.get("/delete/:id", isConnected, actl.deleteSound);
+router.get("/stream/:code", isConnected, actl.setStream);
+router.post("/add_list", isConnected, actl.add_list);
+router.get("/remove_list/:id", isConnected, actl.remove_list);
 
 router.post("/login", loginControl, actl.login);
-
-router.post("/add_list", actl.add_list);
-router.get("/remove_list/:id", actl.remove_list);
-
 
 
 
